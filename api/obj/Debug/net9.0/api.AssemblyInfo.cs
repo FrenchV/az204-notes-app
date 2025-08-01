4e44193da756d6d1a374b1ded733edac5bf841fa
frenchvanilla <ch.arshad.saad@gmail.com>
2025-08-01 13:35:06 -0400

    More organized

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df9d7e024249763714dab82df85839ef99eb92f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd3c5217fdeb6f303d70be858397ad363e3ec402")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
